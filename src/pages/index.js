dcbc16d667e20af8c66be293aa8dc523d16369a5
drifter089 <akshatmittal.0007@gmail.com>
2024-06-11 17:21:08 +0700

    adding same fonts as oet website

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,21 +15,6 @@ export default function index() {
     >
       <main>
         <div class={styles.centered_text}>The OET Handbook</div>
-        <div className={styles.subcontainer}>
-          <p>Welcome to the Open Energy Transition Handbook repository!</p>
-          <p>
-            This repository serves as a centralized hub for managing and
-            tracking policies company-wide for Open Energy Transition.
-          </p>
-          <p>
-            visit OET website{" "}
-            <span>
-              <Link to={"https://openenergytransition.org/"}>here</Link>
-            </span>{" "}
-          </p>
-
-          <p>Website Structure</p>
-        </div>
         <DocsTable />
       </main>
     </Layout>
